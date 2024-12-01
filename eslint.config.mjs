c8b5554421f6b51b9093541b319d116d1bb9ded0
knsnyo <isc963@naver.com>
2024-12-01 23:19:42 +0900

    init: project setting

---
+++ eslint.config.mjs
@@ -0,0 +1,94 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { fixupConfigRules, fixupPluginRules } from '@eslint/compat'
+// eslint-disable-next-line import/no-extraneous-dependencies
+import typescriptEslint from '@typescript-eslint/eslint-plugin'
+// eslint-disable-next-line import/no-extraneous-dependencies
+import prettier from 'eslint-plugin-prettier'
+// eslint-disable-next-line import/no-extraneous-dependencies
+import globals from 'globals'
+// eslint-disable-next-line import/no-extraneous-dependencies
+import tsParser from '@typescript-eslint/parser'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+// eslint-disable-next-line import/no-extraneous-dependencies
+import js from '@eslint/js'
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { FlatCompat } from '@eslint/eslintrc'
+
+// eslint-disable-next-line no-underscore-dangle
+const __filename = fileURLToPath(import.meta.url)
+// eslint-disable-next-line no-underscore-dangle
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+})
+
+export default [
+  ...fixupConfigRules(
+    compat.extends(
+      'airbnb',
+      'airbnb/hooks',
+      'plugin:import/errors',
+      'plugin:import/warnings',
+      'plugin:@typescript-eslint/recommended',
+      'prettier',
+    ),
+  ),
+  {
+    plugins: {
+      '@typescript-eslint': fixupPluginRules(typescriptEslint),
+      prettier,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+
+      parser: tsParser,
+    },
+
+    settings: {
+      'import/parsers': {
+        '@typescript-eslint/parser': ['.ts', '.tsx'],
+      },
+
+      'import/resolver': {
+        typescript: {},
+
+        node: {
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        },
+      },
+    },
+
+    rules: {
+      'import/extensions': [
+        'error',
+        'ignorePackages',
+        {
+          js: 'never',
+          jsx: 'never',
+          ts: 'never',
+          tsx: 'never',
+        },
+      ],
+
+      'react/jsx-filename-extension': [
+        'error',
+        {
+          extensions: ['.jsx', '.tsx'],
+        },
+      ],
+
+      'react/function-component-definition': [
+        2,
+        {
+          namedComponents: ['arrow-function'],
+        },
+      ],
+    },
+  },
+]
---
+++ src/app/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const App: React.FC = () => <p className="text-3xl">123123</p>
+
+export default App
