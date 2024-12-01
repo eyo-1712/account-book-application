9f68cf88c0f2d9dc87f6122dbb966198a4a2e18d
knsnyo <isc963@naver.com>
2024-12-02 00:09:24 +0900

    test: container with appbar

--- src/shared/container/index.stories.tsx
+++ src/shared/container/index.stories.tsx
@@ -1,4 +1,5 @@
 import { Container } from '.'
+import { AppBar } from '../appbar'
 
 export default {
   component: Container,
@@ -17,6 +18,40 @@ export const Default = {
           rerum minima.
         </p>
         <p className="text-3xl">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis
+          perspiciatis error, quaerat, non nobis, quia veritatis illo eos iusto
+          cum quae nihil in repudiandae quibusdam voluptatibus repellat quis
+          rerum minima.
+        </p>
+      </>
+    ),
+  },
+}
+
+export const WithAppBar = {
+  args: {
+    children: (
+      <>
+        <AppBar title="APP BAR" />
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis
+          perspiciatis error, quaerat, non nobis, quia veritatis illo eos iusto
+          cum quae nihil in repudiandae quibusdam voluptatibus repellat quis
+          rerum minima.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis
+          perspiciatis error, quaerat, non nobis, quia veritatis illo eos iusto
+          cum quae nihil in repudiandae quibusdam voluptatibus repellat quis
+          rerum minima.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis
+          perspiciatis error, quaerat, non nobis, quia veritatis illo eos iusto
+          cum quae nihil in repudiandae quibusdam voluptatibus repellat quis
+          rerum minima.
+        </p>
+        <p>
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis
           perspiciatis error, quaerat, non nobis, quia veritatis illo eos iusto
           cum quae nihil in repudiandae quibusdam voluptatibus repellat quis
