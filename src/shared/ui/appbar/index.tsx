5108eefbca594094f1f3148d2aa71071d926d171
knsnyo <isc963@naver.com>
2024-12-02 21:00:24 +0900

    design: appbar

--- src/shared/ui/appbar/index.tsx
+++ src/shared/ui/appbar/index.tsx
@@ -2,9 +2,9 @@ import { IProps } from './type'
 
 export const AppBar = (props: IProps) => (
   <header className="sticky top-0 flex flex-row items-center h-16 bg-white">
-    {props.leading && <nav className="absolute left-0">{props.leading}</nav>}
+    {props.leading && <nav className="absolute left-4">{props.leading}</nav>}
     <p className="flex-1 text-center">{props.title}</p>
-    {props.actions && <nav className="absolute right-0">{props.actions}</nav>}
+    {props.actions && <nav className="absolute right-4">{props.actions}</nav>}
   </header>
 )
 
