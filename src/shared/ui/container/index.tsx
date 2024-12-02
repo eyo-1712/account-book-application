fb418fe8a707914945e3843c6e3b6c33e04bc772
knsnyo <isc963@naver.com>
2024-12-03 00:28:20 +0900

    design: container

--- src/shared/ui/container/index.tsx
+++ src/shared/ui/container/index.tsx
@@ -1,3 +1,5 @@
 export const Container = (props: React.PropsWithChildren) => (
-  <div className="container max-w-screen-sm mx-auto">{props.children}</div>
+  <div className="container max-w-screen-sm mx-auto bg-white">
+    {props.children}
+  </div>
 )
