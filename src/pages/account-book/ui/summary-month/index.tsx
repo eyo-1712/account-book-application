892ea9a0db44c9da00113edb648185c44f68277e
knsnyo <isc963@naver.com>
2024-12-04 23:08:53 +0900

    design: summary month

--- src/pages/account-book/ui/summary-month/index.tsx
+++ src/pages/account-book/ui/summary-month/index.tsx
@@ -5,16 +5,16 @@ export const SummaryMonthPage = () => (
   <Container>
     <AppBar title="2024.11" leading="뒤로가기" actions="추가" />
     <Body>
-      <div className="flex flex-row gap-4">
+      <div className="flex flex-row w-full gap-4">
         <p className="">지출금액</p>
         <p className="text-gray-600">{(10000).toLocaleString()} 원</p>
       </div>
-      <div className="flex flex-row gap-4">
+      <div className="flex flex-row w-full gap-4">
         <p>남은잔고</p>
         <p className="text-blue-600">{(10000).toLocaleString()} 원</p>
       </div>
       <br />
-      <div className="flex flex-col gap-8">
+      <div className="flex flex-col w-full gap-8">
         <ReceiptToday date={new Date()} />
         <ReceiptToday date={new Date()} />
         <ReceiptToday date={new Date()} />
