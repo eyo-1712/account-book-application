a35951a16ad02a0ba8f55d8d8ef186fe5fe357ba
knsnyo <isc963@naver.com>
2024-12-04 22:33:09 +0900

    style: shared body

--- src/shared/ui/body/index.tsx
+++ src/shared/ui/body/index.tsx
@@ -1,3 +1,3 @@
 export const Body = (props: React.PropsWithChildren) => (
-  <div className="flex flex-col py-2 px-4">{props.children}</div>
+  <div className="flex flex-col items-center px-4 py-2">{props.children}</div>
 )
