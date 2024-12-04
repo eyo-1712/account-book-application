81f34802374477ba6a046e10b54f072f4fae0078
knsnyo <isc963@naver.com>
2024-12-04 20:56:26 +0900

    design: account calendar page

--- src/pages/account-book/ui/calendar/index.tsx
+++ src/pages/account-book/ui/calendar/index.tsx
@@ -6,5 +6,20 @@ export const AccountCalendarPage = () => (
   <Container>
     <AppBar title="가계부" />
     <Calendar />
+    <hr className="mx-8" />
+    <div className="flex flex-col items-end py-2 mx-8">
+      <div className="flex flex-row justify-between w-full">
+        <p>시작 잔고</p>
+        <p className="text-blue-600 break-keep">10,000 원</p>
+      </div>
+      <div className="flex flex-row justify-between w-full">
+        <p>지출 금액</p>
+        <p className="text-gray-600 break-keep">20,000 원</p>
+      </div>
+      <div className="flex flex-row justify-between w-full">
+        <p>남은 잔고</p>
+        <p className="text-blue-600 break-keep text-end">300,000,000 원</p>
+      </div>
+    </div>
   </Container>
 )
