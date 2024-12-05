ae88013d123014a85c3c8384545132067d3763af
knsnyo <isc963@naver.com>
2024-12-05 22:59:35 +0900

    feat: react-router

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,23 @@
 import { AccountCalendarPage } from 'pages'
+import { AccountFormPage } from 'pages/account-book/ui/form'
+import { SummaryDetailPage } from 'pages/account-book/ui/summary-detail'
+import { SummaryMonthPage } from 'pages/account-book/ui/summary-month'
+import { LoginPage } from 'pages/auth/ui/login'
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router'
 
-const App: React.FC = () => <AccountCalendarPage />
+const App: React.FC = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/" element={<AccountCalendarPage />} />
+      <Route path="/summary" element={<SummaryMonthPage />} />
+      <Route path="/summary/:id" element={<SummaryDetailPage />} />
+      <Route path="/account/form" element={<AccountFormPage />} />
+      <Route path="/account/form/:id" element={<AccountFormPage />} />
+      <Route />
+    </Routes>
+  </BrowserRouter>
+)
 
 export default App
