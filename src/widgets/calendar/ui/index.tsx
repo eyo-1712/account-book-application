bb38f523f8d3c1df9a31424fae21c18bada384b8
knsnyo <isc963@naver.com>
2024-12-05 23:41:38 +0900

    feat: calendar routing

--- src/widgets/calendar/ui/index.tsx
+++ src/widgets/calendar/ui/index.tsx
@@ -1,14 +1,27 @@
+import { useRouter } from 'shared/lib'
 import { DAY } from './constants'
 import { useLogic } from './logic'
 
-const CalendarBox = (props: React.PropsWithChildren) => (
-  <div className="flex flex-col items-center w-[14.28%] py-2">
+const CalendarBox = (props: {
+  children: React.ReactNode
+  // eslint-disable-next-line react/require-default-props
+  onClick?: () => void
+  // eslint-disable-next-line react/require-default-props
+  disabled?: boolean
+}) => (
+  <button
+    type="button"
+    className="flex flex-col items-center w-[14.28%] py-2"
+    onClick={props.onClick}
+    disabled={props.disabled}
+  >
     {props.children!}
-  </div>
+  </button>
 )
 
 export const Calendar = () => {
   const { value, handler } = useLogic()
+  const { nav } = useRouter()
 
   return (
     <div className="flex flex-col items-center w-full">
@@ -31,7 +44,7 @@ export const Calendar = () => {
       <div className="flex flex-row flex-wrap w-full justify-evenly">
         {value.DATE.map((date, index) => (
           // eslint-disable-next-line react/no-array-index-key
-          <CalendarBox key={index}>
+          <CalendarBox key={index} onClick={nav.summaryMonth()}>
             <p className="text-center">{date}</p>
             {date !== '' && (
               <div className="flex flex-col items-center justify-center h-8">
