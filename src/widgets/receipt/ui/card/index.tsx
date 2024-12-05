01fcc44fd59e6329fea98422c74d8a3033ee4a02
knsnyo <isc963@naver.com>
2024-12-05 23:42:01 +0900

    feat: routing receipt

--- src/widgets/receipt/ui/card/index.tsx
+++ src/widgets/receipt/ui/card/index.tsx
@@ -1,30 +1,39 @@
+import { useRouter } from 'shared/lib'
 import { IProps } from './type'
 
-export const Receipt = (props: IProps) => (
-  <div className="flex flex-row items-center gap-2">
-    <img
-      src={props.src}
-      alt={props.src}
-      style={{
-        width: '3rem',
-        height: '3rem',
-        objectFit: 'cover',
-        aspectRatio: 1,
-        borderRadius: '100%',
-      }}
-    />
+export const Receipt = (props: IProps) => {
+  const { nav } = useRouter()
 
-    <div className="flex flex-col items-start flex-1">
-      {props.price > 0 ? (
-        <p className="font-bold text-blue-600">
-          {props.price.toLocaleString()} 원
-        </p>
-      ) : (
-        <p className="font-bold text-gray-600">
-          {props.price.toLocaleString()} 원
-        </p>
-      )}
-      <p className="text-gray-600">{props.reason}</p>
-    </div>
-  </div>
-)
+  return (
+    <button
+      type="button"
+      className="flex flex-row items-center gap-2"
+      onClick={nav.summaryId('1')}
+    >
+      <img
+        src={props.src}
+        alt={props.src}
+        style={{
+          width: '3rem',
+          height: '3rem',
+          objectFit: 'cover',
+          aspectRatio: 1,
+          borderRadius: '100%',
+        }}
+      />
+
+      <div className="flex flex-col items-start flex-1">
+        {props.price > 0 ? (
+          <p className="font-bold text-blue-600">
+            {props.price.toLocaleString()} 원
+          </p>
+        ) : (
+          <p className="font-bold text-gray-600">
+            {props.price.toLocaleString()} 원
+          </p>
+        )}
+        <p className="text-gray-600">{props.reason}</p>
+      </div>
+    </button>
+  )
+}
