6f754f58657cd087bfa80fd35da836ecaf23debf
knsnyo <isc963@naver.com>
2024-12-06 00:08:19 +0900

    feat: not found page

--- src/pages/not-found/index.tsx
+++ src/pages/not-found/index.tsx
@@ -1,11 +1,18 @@
+import { useRouter } from 'shared/lib'
 import { Container } from 'shared/ui'
 
-export const NotFoundPage = () => (
-  <Container>
-    <main className="flex flex-col items-center justify-center w-screen h-screen">
-      <p>찾을 수 없는 페이지입니다.</p>
-      <br />
-      <button type="button">뒤로가기</button>
-    </main>
-  </Container>
-)
+export const NotFoundPage = () => {
+  const { nav } = useRouter()
+
+  return (
+    <Container>
+      <main className="flex flex-col items-center justify-center w-screen h-screen">
+        <p>찾을 수 없는 페이지입니다.</p>
+        <br />
+        <button type="button" onClick={nav.back}>
+          뒤로가기
+        </button>
+      </main>
+    </Container>
+  )
+}
