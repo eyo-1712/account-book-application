73f983807084ab45bc2ed36723aa9b17f56693df
knsnyo <isc963@naver.com>
2024-12-08 01:41:30 +0900

    chore: stories

---
+++ src/features/router/ui/add-account-detail/index.stories.tsx
@@ -0,0 +1,9 @@
+import { AddAccountDetail } from '.'
+
+export default {
+  component: AddAccountDetail,
+  title: 'AddAccountDetail',
+  tags: ['Features'],
+}
+
+export const Default = {}
---
+++ src/features/router/ui/back/index.stories.tsx
@@ -0,0 +1,9 @@
+import { BackButton } from '.'
+
+export default {
+  component: BackButton,
+  title: 'BackButton',
+  tags: ['Features'],
+}
+
+export const Default = {}
