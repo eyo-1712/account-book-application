fb675d71a3a457bc0804eee015a939f66b14ae77
knsnyo <isc963@naver.com>
2024-12-08 03:38:30 +0900

    chore: storybook root for portal

--- .storybook/decorator/router.tsx
+++ .storybook/decorator/router.tsx
@@ -3,10 +3,18 @@ import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router'
 import { store } from '../../src/app/redux'
 
-export const BrowserRouterDecorator = (Story) => (
-  <Provider store={store}>
-    <BrowserRouter>
-      <Story />
-    </BrowserRouter>
-  </Provider>
-)
+export const BrowserRouterDecorator = (Story) => {
+  if (!document.getElementById('root')) {
+    const portalRoot = document.createElement('div')
+    portalRoot.id = 'root'
+    document.body.appendChild(portalRoot)
+  }
+
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Story />
+      </BrowserRouter>
+    </Provider>
+  )
+}
