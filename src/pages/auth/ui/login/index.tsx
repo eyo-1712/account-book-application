4c8466f25041bf0abd5fa770c5ca3ff992f2ccb8
knsnyo <isc963@naver.com>
2024-12-07 12:23:48 +0900

    design: login page image size

--- src/pages/auth/ui/login/index.tsx
+++ src/pages/auth/ui/login/index.tsx
@@ -7,11 +7,7 @@ export const LoginPage = () => (
       <p className="text-xl font-bold">나만의 가계부 만들기</p>
       <p className="text-gray-300">J들 기절</p>
       <br />
-      <img
-        src="https://picsum.photos/200"
-        alt="onboarding"
-        className="w-full"
-      />
+      <img src="https://picsum.photos/200" alt="onboarding" className="w-1/2" />
       <br />
       <p>SNS로 빠른 시작</p>
       <button type="button">구글 로그인</button>
