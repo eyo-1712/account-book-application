8f8f10b3e484fa02f460ac6324b4b25e6db173d4
knsnyo <isc963@naver.com>
2024-12-09 02:19:28 +0900

    refactor: button

--- src/shared/ui/button/index.tsx
+++ src/shared/ui/button/index.tsx
@@ -1,12 +1,12 @@
 import { BUTTON_COLOR } from './constant'
 import { IProps } from './type'
 
-export const Button = ({ children, ...props }: IProps) => (
+export const Button = ({ children, color, ...props }: IProps) => (
   // eslint-disable-next-line react/button-has-type
   <button
     className={[
       'font-bold disabled:text-gray-300',
-      `${BUTTON_COLOR[props.color]}`,
+      `${BUTTON_COLOR[color]}`,
     ].join(' ')}
     // eslint-disable-next-line react/jsx-props-no-spreading
     {...props}
