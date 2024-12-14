0d8201b11c966d8473ddbea81aea2851f43d9534
knsnyo <isc963@naver.com>
2024-12-15 00:44:49 +0900

    design: analysis page

--- src/pages/analysis/ui/index.tsx
+++ src/pages/analysis/ui/index.tsx
@@ -1,11 +1,45 @@
+import {
+  Bar,
+  BarChart,
+  Legend,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  XAxis,
+  YAxis,
+} from 'recharts'
 import { AppBar, Body, Container } from 'shared/ui'
 import { Hamburger } from 'widgets'
+import { BAR_MOCK_DATA, LINE_MOCK_DATA } from './mock.data'
 
 export const AnalysisPage = () => (
   <Container>
     <AppBar title="지출 분석" leading={<Hamburger />} />
-    <Body>
-      <p>TEST</p>
+    <Body center>
+      <p>이번달 지출 내역</p>
+      <p className="text-xs text-gray-400">지난달보다 ___원 덜 쓰는 중</p>
+      <br />
+      <ResponsiveContainer width="75%" height={200}>
+        <LineChart data={LINE_MOCK_DATA}>
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Line type="monotone" dataKey="pv" stroke="#2563eb" dot={false} />
+          <Line type="monotone" dataKey="amt" stroke="#9ca3af" dot={false} />
+          <Legend />
+        </LineChart>
+      </ResponsiveContainer>
+      <br />
+      <br />
+      <p>카테고리 별 지출 내역</p>
+      <p className="text-xs text-gray-400">어디에 제일 많이 쓰는 중</p>
+      <br />
+      <ResponsiveContainer width="75%" height={200}>
+        <BarChart data={BAR_MOCK_DATA}>
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Bar type="monotone" dataKey="pv" fill="#9ca3af" />
+        </BarChart>
+      </ResponsiveContainer>
     </Body>
   </Container>
 )
---
+++ src/pages/analysis/ui/mock.data.ts
@@ -0,0 +1,54 @@
+export const LINE_MOCK_DATA = [
+  { name: 0, uv: 0, pv: 0, amt: 0 },
+  { name: 1, uv: 100, pv: 500, amt: 1000 },
+  { name: 2, uv: 500, pv: 2200, amt: 2400 },
+  { name: 3, uv: 600, pv: 2000, amt: 2800 },
+  { name: 4, uv: 700, pv: 2100, amt: 3000 },
+  { name: 5, uv: 700, pv: 2100, amt: 3000 },
+  { name: 6, uv: 700, pv: 2100, amt: 3000 },
+  { name: 7, uv: 700, pv: 2100, amt: 3000 },
+  { name: 8, uv: 700, pv: 2100, amt: 3000 },
+  { name: 9, uv: 700, pv: 2100, amt: 3000 },
+  { name: 10, uv: 700, pv: 2100, amt: 3000 },
+  { name: 11, amt: 3000 },
+  { name: 12, amt: 3000 },
+  { name: 13, amt: 3000 },
+]
+
+export const BAR_MOCK_DATA = [
+  {
+    name: 'A',
+    uv: 4000,
+    pv: 2400,
+  },
+  {
+    name: 'B',
+    uv: 3000,
+    pv: 1398,
+  },
+  {
+    name: 'C',
+    uv: 2000,
+    pv: 9800,
+  },
+  {
+    name: 'D',
+    uv: 2780,
+    pv: 3908,
+  },
+  {
+    name: 'E',
+    uv: 1890,
+    pv: 4800,
+  },
+  {
+    name: 'F',
+    uv: 2390,
+    pv: 3800,
+  },
+  {
+    name: 'G',
+    uv: 3490,
+    pv: 4300,
+  },
+]
