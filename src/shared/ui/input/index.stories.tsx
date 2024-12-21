d7b020984ba65aea161521648c4d7169fceec865
knsnyo <isc963@naver.com>
2024-12-21 23:54:37 +0900

    chore: input storybook

---
+++ src/shared/ui/input/index.stories.tsx
@@ -0,0 +1,13 @@
+import { Input } from '.'
+
+export default {
+  component: Input,
+  title: 'Input',
+  tags: ['Shared'],
+}
+
+export const Default = {
+  args: {
+    placeholder: '입력하세염',
+  },
+}
