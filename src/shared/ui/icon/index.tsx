f409bf29abe4ef179291f03d2fafe0e03076af82
knsnyo <isc963@naver.com>
2025-01-14 21:46:42 +0900

    refactor: antd -> lucide

--- src/shared/ui/icon/index.tsx
+++ src/shared/ui/icon/index.tsx
@@ -1,35 +1,35 @@
 import {
-  CalendarOutlined,
-  CloseOutlined,
-  CreditCardOutlined,
-  FolderAddOutlined,
-  FolderOpenOutlined,
-  LeftOutlined,
-  LineChartOutlined,
-  MenuOutlined,
-  MinusSquareOutlined,
-  RightOutlined,
-  WalletOutlined,
-} from '@ant-design/icons'
+  Calendar,
+  ChartSpline,
+  ChevronLeft,
+  ChevronRight,
+  CreditCard,
+  Folder,
+  FolderPlus,
+  Menu,
+  Minus,
+  Wallet,
+  X,
+} from 'lucide-react'
 import { IProps } from './type'
 
 export const Icon = (props: IProps) => {
-  const AntD = {
-    back: LeftOutlined,
-    left: LeftOutlined,
-    right: RightOutlined,
-    close: CloseOutlined,
-    chart: LineChartOutlined,
-    calendar: CalendarOutlined,
-    card: CreditCardOutlined,
-    category: FolderOpenOutlined,
-    'create-category': FolderAddOutlined,
-    menu: MenuOutlined,
-    'remove-form': MinusSquareOutlined,
-    wallet: WalletOutlined,
+  const Lucide = {
+    back: ChevronLeft,
+    left: ChevronLeft,
+    right: ChevronRight,
+    close: X,
+    chart: ChartSpline,
+    calendar: Calendar,
+    card: CreditCard,
+    category: Folder,
+    'create-category': FolderPlus,
+    menu: Menu,
+    'remove-form': Minus,
+    wallet: Wallet,
   }[props.type]
 
-  return <AntD />
+  return <Lucide />
 }
 
 /**
