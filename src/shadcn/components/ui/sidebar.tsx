f1b0c63f7d8f5fec63b5c891176efea4fc2f5877
knsnyo <isc963@naver.com>
2025-01-15 20:31:04 +0900

    refactor: sidebar trigger

--- src/shadcn/components/ui/sidebar.tsx
+++ src/shadcn/components/ui/sidebar.tsx
@@ -3,7 +3,7 @@
 /* eslint-disable no-shadow */
 import { Slot } from '@radix-ui/react-slot'
 import { VariantProps, cva } from 'class-variance-authority'
-import { PanelLeft } from 'lucide-react'
+import { Menu } from 'lucide-react'
 import * as React from 'react'
 
 import { Button } from 'shadcn/components/ui/button'
@@ -287,7 +287,7 @@ const SidebarTrigger = React.forwardRef<
       }}
       {...props}
     >
-      <PanelLeft />
+      <Menu />
       <span className="sr-only">Toggle Sidebar</span>
     </Button>
   )
