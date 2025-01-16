19ef27beb423a53246565db065b7a0cca1f23d11
knsnyo <isc963@naver.com>
2025-01-16 22:43:49 +0900

    refactor: icon

--- src/shared/ui/icon/index.tsx
+++ src/shared/ui/icon/index.tsx
@@ -12,27 +12,25 @@ import {
   Wallet,
   X,
 } from 'lucide-react'
+import { match } from 'ts-pattern'
 import { IconProps } from './type'
 
-export const Icon = (props: IconProps) => {
-  const Lucide = {
-    back: ChevronLeft,
-    left: ChevronLeft,
-    right: ChevronRight,
-    close: X,
-    chart: ChartSpline,
-    calendar: Calendar,
-    card: CreditCard,
-    category: Folder,
-    'create-category': FolderPlus,
-    menu: Menu,
-    'remove-form': Minus,
-    wallet: Wallet,
-    logout: LogOut,
-  }[props.type]
-
-  return <Lucide className="size-4" />
-}
+export const Icon = ({ type }: IconProps) =>
+  match(type)
+    .with('back', () => <ChevronLeft className="size-4" />)
+    .with('left', () => <ChevronLeft className="size-4" />)
+    .with('right', () => <ChevronRight className="size-4" />)
+    .with('close', () => <X className="size-4" />)
+    .with('chart', () => <ChartSpline className="size-4" />)
+    .with('calendar', () => <Calendar className="size-4" />)
+    .with('card', () => <CreditCard className="size-4" />)
+    .with('category', () => <Folder className="size-4" />)
+    .with('create-category', () => <FolderPlus className="size-4" />)
+    .with('menu', () => <Menu className="size-4" />)
+    .with('remove-form', () => <Minus className="size-4" />)
+    .with('wallet', () => <Wallet className="size-4" />)
+    .with('logout', () => <LogOut className="size-4" />)
+    .exhaustive()
 
 /**
  * https://ant.design/components/icon
