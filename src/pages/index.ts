8ba22d22681b953197564c1771d22a73738c0481
knsnyo <isc963@naver.com>
2025-01-18 22:48:04 +0900

    rename: ui

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,23 +1,23 @@
 import { withAuth, withNoAuth } from 'features/user'
-import { AccountDetailPage as accountDetailPage } from './account/detail'
-import { AccountFormPage as accountFormPage } from './account/form'
-import { AccountListPage as accountListPage } from './account/list'
-import { AccountTransferPage as accountTransferPage } from './account/transfer'
+import { AccountDetailPage as accountDetailPage } from './account/ui/detail'
+import { AccountFormPage as accountFormPage } from './account/ui/form'
+import { AccountListPage as accountListPage } from './account/ui/list'
+import { AccountTransferPage as accountTransferPage } from './account/ui/transfer'
 import { AnalysisPage as analysisPage } from './analysis/ui'
 import { LoginPage as loginPage } from './auth/ui/login'
 import { CategoryDetailPage as categoryDetailPage } from './category/ui/detail'
 import { CategoryFormPage as categoryFormPage } from './category/ui/form'
 import { CategoryListPage as categoryListPage } from './category/ui/list'
-import { FixedDetailPage as fixedDetailPage } from './fixed/detail'
-import { FixedFormPage as fixedFormPage } from './fixed/form'
-import { FixedListPage as fixedListPage } from './fixed/list'
+import { FixedDetailPage as fixedDetailPage } from './fixed/ui/detail'
+import { FixedFormPage as fixedFormPage } from './fixed/ui/form'
+import { FixedListPage as fixedListPage } from './fixed/ui/list'
 import { SummaryCalendarPage as summaryCalendarPage } from './summary/ui/calendar'
 import { SummaryFormPage as summaryFormPage } from './summary/ui/form'
 import { SummaryDetailPage as summaryDetailPage } from './summary/ui/summary-detail'
 import { SummaryMonthPage as summaryMonthPage } from './summary/ui/summary-month'
 
 // common
-export { NotFoundPage } from './not-found'
+export { NotFoundPage } from './not-found/ui'
 
 // no auth
 export const LoginPage = withNoAuth(loginPage)
