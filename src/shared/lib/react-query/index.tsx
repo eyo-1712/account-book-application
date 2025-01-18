161fa53eb8c759bb9c4b8f7c082c1f57f859aa82
knsnyo <isc963@naver.com>
2025-01-18 23:37:05 +0900

    feat: setting react-query stale time

--- src/shared/lib/react-query/index.tsx
+++ src/shared/lib/react-query/index.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { staleTime: 1000 * 60 * 60 * 24 } },
+})
 
 export const ReactQueryProvider = ({
   children,
