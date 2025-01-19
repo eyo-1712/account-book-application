67fb253863ed8a25502ea47d1630dcceab345552
knsnyo <isc963@naver.com>
2025-01-19 20:37:02 +0900

    fix: nav back button

--- src/features/router/ui/back/index.tsx
+++ src/features/router/ui/back/index.tsx
@@ -1,11 +1,17 @@
+import { Button } from 'shadcn/components/ui/button'
 import { useRouter } from 'shared/lib'
 import { Icon } from 'shared/ui'
 
 export const Back = () => {
-  const { nav } = useRouter()
+  const router = useRouter()
   return (
-    <button type="button" onClick={nav.back}>
+    <Button
+      variant="ghost"
+      size="icon"
+      className="h-7 w-7"
+      onClick={router.nav.back}
+    >
       <Icon type="back" />
-    </button>
+    </Button>
   )
 }
