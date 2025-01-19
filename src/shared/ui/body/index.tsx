7b22cbd7f2b9a4275c0aabc50d0566eaa233efdc
knsnyo <isc963@naver.com>
2025-01-19 22:13:19 +0900

    fix: body style

--- src/shared/ui/body/index.tsx
+++ src/shared/ui/body/index.tsx
@@ -3,7 +3,7 @@ import { BodyProps } from './type'
 export const Body = (props: BodyProps) => (
   <div
     className={[
-      'flex flex-col w-full px-4 py-2',
+      'flex flex-col w-full px-7 py-2',
       `${props?.center && 'items-center'}`,
     ].join(' ')}
   >
