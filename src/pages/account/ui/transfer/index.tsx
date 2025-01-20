a3c086bac4e464a65ac3351d0c133add559238f7
knsnyo <isc963@naver.com>
2025-01-20 22:04:47 +0900

    feat: init account transfer select

--- src/pages/account/ui/transfer/index.tsx
+++ src/pages/account/ui/transfer/index.tsx
@@ -1,19 +1,29 @@
+import { useApiFetchAccounts } from 'entities'
 import { Back } from 'features/router'
 import { AppBar, Body, Button, Container, Form, Input, Select } from 'shared/ui'
 
-export const AccountTransferPage = () => (
-  <Container>
-    <AppBar
-      title="계좌 이체"
-      leading={<Back />}
-      actions={<Button color="blue">이체</Button>}
-    />
-    <Body>
-      <Form>
-        <Select placeholder="보낼 계좌를 선택하세요" options={[]} />
-        <Select placeholder="받을 계좌를 선택하세요" options={[]} />
-        <Input type="number" placeholder="금액을 입력하세요." />
-      </Form>
-    </Body>
-  </Container>
-)
+export const AccountTransferPage = () => {
+  const { data, isSuccess } = useApiFetchAccounts()
+
+  const render = (isSuccess ? data : []).map((r) => ({
+    label: r.name,
+    value: `${r.id}`,
+  }))
+
+  return (
+    <Container>
+      <AppBar
+        title="계좌 이체"
+        leading={<Back />}
+        actions={<Button color="blue">이체</Button>}
+      />
+      <Body>
+        <Form>
+          <Select placeholder="보낼 계좌를 선택하세요" options={render} />
+          <Select placeholder="받을 계좌를 선택하세요" options={render} />
+          <Input type="number" placeholder="금액을 입력하세요." />
+        </Form>
+      </Body>
+    </Container>
+  )
+}
